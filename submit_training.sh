d4df978cd4a3e550a54ad26d59e03974ec3c0bf8
Onat <ozeronat@umich.edu>
2025-01-12 14:38:43 -0500

    improved batch script

--- submit_training.sh
+++ submit_training.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
+  
+# SLURM directives
 
-#Indicate the account to charge the job to
-#SBATCH --account=mdatascience_team
+#SBATCH --account=mdatascience_team  # Account to charge the job to
+#SBATCH --job-name=train_pokerbot    # Job name
+#SBATCH --mail-user=ozeronat@umich.edu  # Email for notifications
+#SBATCH --mail-type=END             # Notification type: send email when job ends
+#SBATCH --nodes=1                   # Number of nodes
+#SBATCH --ntasks-per-node=1         # Number of tasks per node
+#SBATCH --cpus-per-task=2           # Number of CPUs per task
+#SBATCH --mem=184320M                   # Total memory for the job
+#SBATCH --time=14:00:00             # Maximum runtime
+#SBATCH --gpus=1                    # Number of GPUs
+#SBATCH --partition=gpu             # Partition to use
 
-#Indicate a name to give the job. This can be anything, it's just a way to be able to easily track different jobs
-#SBATCH --job-name=train_pokerbot
 
-#Indicate where to send information about the job
-#SBATCH --mail-user=ozeronat@umich.edu
+# Environment setup
 
-#Indicate how often you want info about the job. In this case, you will receive an email when the job has ended
-#SBATCH --mail-type=END
+module purge                         # Clean environment
+module load python/3.9 cuda/11.8     # Load necessary modules (update versions as required)
 
-#Indicate how many nodes to run the job on
-#SBATCH --nodes=1
+# Check GPU and CUDA availability
+echo "CUDA and GPU Details:"
 
-#Indicate how many tasks to run on each node
-#SBATCH --ntasks-per-node=1
-
-#SBATCH --cpus-per-task=8              # Number of CPUs per task
-#SBATCH --mem-per-cpu=8000m            # Memory per CPU (8GB)
-#SBATCH --time=10:00:00                # Maximum run time
-
-#SBATCH --gpus=1                       # Number of GPUs to allocate
-#SBATCH --constraint=gpu_a100         #really not sure that this is a thing
-#SBATCH --partition=spgpu              
-
-#Get rid of any modules that might still be running
-module purge
-
-# Create virtual env
-# python3 -m venv venv #Only needs to be ran once on device
-source venv/bin/activate
-
-# pip3 install-r requirements.txt #Same thing here
-
-# Run the desired program
 nvidia-smi
-python3 train_model.py
+nvcc --version
+
+
+# Activate virtual environment
+source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+
+
+# Ensure required packages are installed
+
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu118
+pip install -r requirements.txt
+
+# Run the program
+python3 train_model.py --iterations 100 --k 100
+
+# Post-job cleanup (optional)
+deactivate
